f8c2b16c94e50aaad3d5ff409a3b8dbb6c2802cf
Alex Razygraev <rav200@mail.ru>
2023-01-29 11:52:04 +0300

    Add autotests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,11 +4,37 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @org.junit.Test
-    public void remain() {
+    public void testRemainWithCashback() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void testRemain() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainJunit5(){
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected,actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void testRemainWithCashbackJunit5() {
         int amount = 900;
         int expected = 100;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
         assertEquals(expected,actual);
     }
+
 }
