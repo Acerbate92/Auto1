5d11a3187a7aece67049b40400a4c4939be2e6cf
Alex Razygraev <rav200@mail.ru>
2023-01-29 11:35:46 +0300

    Add autotest

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,10 +6,19 @@ import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
-    public void remain() {
+    @org.testng.annotations.Test
+    public void testRemainWithCashback() {
         int amount = 900;
         int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        int amount = 1000;
+        int expected = 0;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
         assertEquals(actual,expected);
